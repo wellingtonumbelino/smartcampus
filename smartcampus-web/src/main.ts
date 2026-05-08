f09275432b61fc6a8184b4c7c3d729df2b6a9b56
Wellington Umbelino <wellington@insightlab.ufc.br>
2026-05-08 16:24:14 -0300

    feat: adjust layout in Dashboard

--- smartcampus-web/src/main.ts
+++ smartcampus-web/src/main.ts
@@ -5,7 +5,15 @@ import App from "./App.vue";
 import router from "./router";
 import HeaderTitle from "./ui/HeaderTitle.vue";
 import "./assets/main.scss";
-import { Button, Column, DataTable, Dialog, InputText } from "primevue";
+import {
+  Button,
+  Card,
+  Column,
+  DataTable,
+  Dialog,
+  Divider,
+  InputText,
+} from "primevue";
 import { createPinia } from "pinia";
 
 const app = createApp(App);
@@ -13,6 +21,8 @@ const app = createApp(App);
 app.component("Button", Button);
 app.component("DataTable", DataTable);
 app.component("Dialog", Dialog);
+app.component("Divider", Divider);
+app.component("Card", Card);
 app.component("Column", Column);
 app.component("HeaderTitle", HeaderTitle);
 app.component("InputText", InputText);
--- smartcampus-web/src/views/Dashboard/Dashboard.vue
+++ smartcampus-web/src/views/Dashboard/Dashboard.vue
@@ -1,3 +1,80 @@
+<template>
+  <div class="dashboard">
+    <div class="dashboard-info-actions">
+      <Card class="card-number-info">
+        <template #title>TOTAL IOT DEVICES</template>
+        <template #content>
+          <p>{{ totalDevices }}</p>
+        </template>
+      </Card>
+
+      <Card class="card-number-info">
+        <template #title>PHYSICAL ROOMS</template>
+        <template #content>
+          <p>{{ roomStore.totalRooms }}</p>
+        </template>
+      </Card>
+
+      <Card class="card-detail-info">
+        <template #title>ACTIONS</template>
+        <template #content>
+          <Divider />
+          <div class="actions-button">
+            <Button
+              label="Run Planner"
+              icon="pi pi-play-circle"
+              :loading="loading"
+              @click="runPlannerService"
+            />
+            <Button
+              label="View Schedule"
+              icon="pi pi-eye"
+              :loading="scheduleLoading"
+              @click="viewSchedule"
+            />
+          </div>
+        </template>
+      </Card>
+
+      <Card class="card-detail-info">
+        <template #title>STATUS</template>
+        <template #content>
+          <div v-if="plannerStatusResult" class="status-info-container">
+            <span class="status-info">
+              <label>Execution ID</label>
+              <p>{{ plannerStatusResult?.jobId }}</p>
+            </span>
+            <span class="status-info">
+              <label>Execution Time</label>
+              <p>{{ plannerStatusResult?.executionTime }}</p>
+            </span>
+            <span class="status-info">
+              <label>N°. Actions Generated</label>
+              <p>{{ plannerStatusResult?.actionsCount }}</p>
+            </span>
+            <Divider />
+            <div class="status-card-footer">
+              <i class="pi pi-check-circle" />
+              <p>Execution completed without errors.</p>
+            </div>
+          </div>
+          <p v-else>No planner status available.</p>
+        </template>
+      </Card>
+    </div>
+
+    <section class="dashboard-schedule">
+      <h2>Scheduled Actions</h2>
+      <ul v-if="scheduleActions.length > 0">
+        <li v-for="(action, index) in scheduleActions" :key="index">
+          {{ action.jobId }}
+        </li>
+      </ul>
+      <p v-else>No scheduled actions available.</p>
+    </section>
+  </div>
+</template>
+
 <script setup lang="ts">
 import { onMounted, ref } from "vue";
 import mockDevices from "../../_mock/devices.json";
@@ -50,71 +127,53 @@ async function viewSchedule() {
 }
 </script>
 
-<template>
-  <div class="dashboard">
-    <section class="dashboard-section">
-      <h2>Resumo</h2>
-      <div class="cards">
-        <div class="card">
-          <h3>Total Devices</h3>
-          <p>{{ totalDevices }}</p>
-        </div>
-        <div class="card">
-          <h3>Total Rooms</h3>
-          <p>{{ roomStore.totalRooms }}</p>
-        </div>
-      </div>
-    </section>
-    <section class="dashboard-section">
-      <h2>Actions</h2>
-      <div class="dashboard-actions">
-        <Button
-          label="Run Planner"
-          icon="pi pi-play"
-          :loading="loading"
-          @click="runPlannerService"
-        />
-        <Button
-          label="View Schedule"
-          icon="pi pi-eye"
-          :loading="scheduleLoading"
-          @click="viewSchedule"
-        />
-      </div>
-    </section>
-    <section class="dashboard-status">
-      <h2>Status</h2>
-      <div class="dashboard-status-info">
-        <div v-if="plannerStatusResult">
-          <p>Execution Time: {{ plannerStatusResult.executionTime }}</p>
-          <p>Job ID: {{ plannerStatusResult.jobId }}</p>
-          <pMessage
-            :severity="
-              plannerStatusResult.status === 'Success' ? 'success' : 'error'
-            "
-            :summary="plannerStatusResult.message"
-          />
-          <p>Actions Count: {{ plannerStatusResult.actionsCount }}</p>
-        </div>
-        <div v-else>
-          <p>No planner status available.</p>
-        </div>
-      </div>
-    </section>
-    <section class="dashboard-schedule">
-      <h2>Scheduled Actions</h2>
-      <ul v-if="scheduleActions.length > 0">
-        <li v-for="(action, index) in scheduleActions" :key="index">
-          {{ action.jobId }}
-        </li>
-      </ul>
-      <p v-else>No scheduled actions available.</p>
-    </section>
-  </div>
-</template>
-
 <style scoped lang="scss">
 .dashboard {
+  .dashboard-info-actions {
+    display: flex;
+    gap: 2rem;
+
+    .card-number-info {
+      width: 25%;
+    }
+    .card-detail-info {
+      width: 50%;
+
+      .actions-button {
+        display: flex;
+        gap: 1rem;
+        width: 100%;
+
+        .p-button {
+          flex: 1;
+          height: 2.5rem;
+        }
+      }
+
+      .status-info-container {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        p {
+          margin: 0;
+        }
+
+        .status-info {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+
+        .status-card-footer {
+          display: flex;
+          gap: 1rem;
+          align-items: center;
+        }
+      }
+    }
+  }
+
   .dashboard-section {
     h3 {
       margin: 0.5rem;
