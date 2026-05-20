8257b579a2ce6d8c9c101a12cf15ee7c4816d320
Wellington Umbelino <wellington@insightlab.ufc.br>
2026-05-20 09:26:43 -0300

    feat: update side menu

--- smartcampus-web/src/core/layout/SideMenu.vue
+++ smartcampus-web/src/core/layout/SideMenu.vue
@@ -1,25 +1,30 @@
 <template>
   <aside id="side-menu" role="menu">
-    <nav class="side-menu-nav">
-      <div class="system-info">
-        <span class="title">APMS</span>
-        <span class="version">v{{ version }}</span>
-      </div>
-      <ul>
-        <li v-for="item in sideMenuItems" :key="item.label">
-          <router-link :to="item.route" class="item-link">
-            <i :class="item.icon" />
+    <Menu :model="sideMenuItems">
+      <template #start>
+        <div class="system-info-container">
+          <Avatar label="A" />
+          <div class="system-info">
+            <span class="system-name-acronym">APMS</span>
+            <span class="system-version">v{{ version }}</span>
+          </div>
+        </div>
+      </template>
+
+      <template #item="{ item, props }">
+        <router-link
+          v-if="item.route"
+          v-slot="{ href, navigate }"
+          :to="item.route"
+          custom
+        >
+          <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+            <span :class="item.icon" />
             <span>{{ item.label }}</span>
-          </router-link>
-        </li>
-      </ul>
-      <span class="end-options">
-        <span class="sign-out-option item-link">
-          <i class="pi pi-sign-out" />
-          <span>Sign Out</span>
-        </span>
-      </span>
-    </nav>
+          </a>
+        </router-link>
+      </template>
+    </Menu>
   </aside>
 </template>
 
@@ -28,75 +33,42 @@ import { useNavigation } from "./composables/useNavigation";
 import { version } from "../../../package.json";
 
 const { sideMenuItems } = useNavigation();
-
-console.log("Items side menu", sideMenuItems);
 </script>
 
 <style scoped lang="scss">
 #side-menu {
-  border-right: 1px solid #cecece;
   height: 100%;
 
-  .item-link {
-    display: flex;
-    gap: 0.5rem;
-    align-items: center;
-    text-decoration: none;
-    padding: 0.75rem 1rem;
-    min-width: 9.375rem;
-  }
-
-  .side-menu-nav {
-    display: flex;
-    flex-direction: column;
+  .p-menu {
     height: 100%;
 
-    .system-info {
-      padding: 1.12rem 1rem;
-      display: flex;
-      flex-direction: column;
-
-      .title {
-        font-size: 1rem;
-        font-weight: 600;
-        color: black;
-      }
-
-      .version {
-        font-size: 0.875rem;
-        color: #666;
-      }
-    }
-
-    ul {
-      list-style: none;
-      margin: 0;
-      padding: 0.25rem;
-
-      li {
-        &:hover {
-          background-color: #cecece;
-        }
-      }
-    }
-
-    .end-options {
-      display: flex;
-      float: bottom;
-      flex-direction: column;
-      gap: 0.5rem;
-      padding: 0.5rem 0.25rem;
-      border-top: 1px solid #cecece;
-      margin-top: auto;
-
-      .sign-out-option {
-        cursor: pointer;
+    .p-menu-start {
+      .system-info-container {
+        display: flex;
+        gap: 0.625rem;
         padding: 0.75rem 1rem;
+        align-items: center;
 
-        &:hover {
-          background-color: #cecece;
+        .system-info {
+          display: flex;
+          flex-direction: column;
+
+          .system-name-acronym {
+            font-size: 0.875rem;
+            font-weight: 500;
+            letter-spacing: 0.025rem;
+          }
+
+          .system-version {
+            font-size: 0.75rem;
+            font-weight: 300;
+          }
         }
       }
+    }
+
+    &.p-component {
+      border-radius: 0;
     }
   }
 }
