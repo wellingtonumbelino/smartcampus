ae7757c9c157706acd925b27063b29d2e29f0e7a
Wellington Umbelino <wellington@insightlab.ufc.br>
2026-07-03 16:35:22 -0300

    feat: improvinment alembic config

--- smart-campus-service/alembic/env.py
+++ smart-campus-service/alembic/env.py
@@ -1,9 +1,16 @@
+import sys
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+sys.path.append(
+    os.path.join(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+)
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,7 +25,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.models._base_model import Base
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +73,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
--- smart-campus-service/app/models/user_model.py
+++ smart-campus-service/app/models/user_model.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 from sqlalchemy import String, DateTime, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 
-
-class Base(DeclarativeBase):
-    pass
+from _base_model import Base
 
 
 class User(Base):
