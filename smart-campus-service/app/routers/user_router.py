35973c908d2eee46fef32c5bd45168df32620b50
Wellington Umbelino <wellington@insightlab.ufc.br>
2026-07-03 16:02:35 -0300

    fix: rename user router

--- smart-campus-service/app/routers/user_router.py
+++ smart-campus-service/app/routers/user_router.py
@@ -4,17 +4,17 @@ from app.dependencies.user_deps import get_user_service
 from app.schemas.user_schema import UserResponse, UserCreate
 from app.services.user_service import UserService
 
-router = APIRouter(prefix="/users", tags=["User"])
+user_router = APIRouter(prefix="/users", tags=["User"])
 
 
-@router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+@user_router.post("/", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 async def create_user(
     user_data: UserCreate, user_service: UserService = Depends(get_user_service)
 ):
     return await user_service.create_user(user_data)
 
 
-@router.get("/{email}", response_model=UserResponse)
+@user_router.get("/{email}", response_model=UserResponse)
 async def get_user_by_email(
     email: str, user_service: UserService = Depends(get_user_service)
 ) -> UserResponse:
