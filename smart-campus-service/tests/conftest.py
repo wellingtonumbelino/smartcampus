5f9afc9431c87c0e97ec32cdc15064399a48be49
Wellington Umbelino <wellington@insightlab.ufc.br>
2026-07-03 11:40:20 -0300

    refactor: remove all unit tests

--- smart-campus-service/tests/conftest.py
+++
@@ -1,5 +0,0 @@
-import sys
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(ROOT_DIR))
--- smart-campus-service/tests/interfaces/http/fakes/fake_use_case.py
+++
@@ -1,3 +0,0 @@
-class FakeGeneratePDDLUseCase:
-    def execute(self, input_data):
-        return "FAKE_PDDL_FROM_API"
